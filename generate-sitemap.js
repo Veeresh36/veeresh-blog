4d496d51b9366767d97019e9b3221ed13e55391f
Veeresh Bashetti <veereshbashetti121@gmail.com>
2026-06-28 19:54:07 +0530

    production ready to launch

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const DOMAIN = "https://veeresh-blog.vercel.app";
+const DOMAIN = "https://veereshbashetti.com/";
 
 const blogsDir = path.join(__dirname, "public", "blogs");
 
